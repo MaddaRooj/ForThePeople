c7609ed3e5427e97b97f9a60f1a49180ed0dca18
Addam Joor <38664958+MaddaRooj@users.noreply.github.com>
2019-09-04 11:18:44 -0500

    first commit

---
+++ ForThePeople/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ForThePeople.Models;
+using Microsoft.Extensions.Configuration;
+using System.Net.Http;
+
+namespace ForThePeople.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly string _repsUrl = "https://www.googleapis.com/civicinfo/v2/representatives?address=nashville&key=";
+        private readonly string _electionsUrl = "https://www.googleapis.com/civicinfo/v2/elections?key=";
+        private readonly IConfiguration _config;
+
+        public HomeController(IConfiguration config)
+        {
+            _config = config;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var elections = await GetElectionsAsync();
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private async Task<Election> GetElectionsAsync()
+        {
+            var key = _config["ApiKeys:GoogleCivicApi"];
+            var url = $"{_electionsUrl}{key}";
+            var client = new HttpClient();
+            var response = await client.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var election = await response.Content.ReadAsAsync<Election>();
+                return election;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ ForThePeople/Models/Representative.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForThePeople.Models
+{
+    public class Representative
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Party { get; set; }
+    }
+}
