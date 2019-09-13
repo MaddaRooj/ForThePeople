c5938f2fa5826effec18b111427db903eae96433
Addam Joor <38664958+MaddaRooj@users.noreply.github.com>
2019-09-13 11:50:26 -0500

    added note model

---
+++ ForThePeople/Models/Note.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForThePeople.Models
+{
+    public class Note
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string UserId { get; set; }
+        public string RepId { get; set; }
+        public string BillId { get; set; }
+    }
+}
