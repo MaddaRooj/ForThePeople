26cf62a277bdfee69f578427f651938c6be14f5e
Addam Joor <38664958+MaddaRooj@users.noreply.github.com>
2019-09-13 15:08:21 -0500

    basic styling changes

--- ForThePeople/Models/Note.cs
+++ ForThePeople/Models/Note.cs
@@ -10,8 +10,10 @@ namespace ForThePeople.Models
     {
         [Key]
         public int Id { get; set; }
+        [Required]
         public string Title { get; set; }
         public string Content { get; set; }
+        [Required]
         public string UserId { get; set; }
         public string RepId { get; set; }
         public string BillId { get; set; }
